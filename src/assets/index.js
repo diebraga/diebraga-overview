be205fa2e5e2fac1ee6ddf95af5daeb00a464ce7
Diego Braga <diegobraga@hertz.com>
2023-03-12 13:45:55 +0000

    removed unused files

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,21 +1,14 @@
-import backend from "./backend.png";
-import mobile from "./mobile.png";
-import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 import kos from "./kos.webp";
 
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
@@ -34,10 +27,6 @@ import express from "./tech/express.jpeg";
 import django from "./tech/django.png";
 import grpc from "./tech/grpc.png";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
 import hertz from "./company/hertz.png";
 
 import carrent from "./carrent.png";
@@ -46,30 +35,19 @@ import tripguide from "./tripguide.png";
 
 export {
   python,
-  backend,
   rn,
-  mobile,
-  web,
   github,
   menu,
   close,
   css,
-  docker,
-  figma,
   git,
   html,
   javascript,
-  mongodb,
   nodejs,
   reactjs,
-  redux,
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   carrent,
   jobit,
   tripguide,
